5c7d97c6c2cd3c744bd925a2913f3faf82be59d0
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2023-10-12 11:17:11 +0100

    MySQL advanced

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+--script that creates a table users
+--with unique email column
+CREATE TABLE If NOT EXISTS `users` (  
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255)
+);
